a0a13d57a7ae3f4723163f778a84c0132e0bdc2e
anuj <anujpy.acct@gmail.com>
2023-11-29 08:29:04 +0530

    print z basic printing

---
+++ src/basic_questions/Print_Z.java
@@ -0,0 +1,20 @@
+package basic_questions;
+
+public class Print_Z {
+    public static void main(String[] args) {
+//        print the following pattern
+
+//        *****
+//           *
+//          *
+//         *
+//        *****
+
+        System.out.println("* * * * *");
+        System.out.println("      * ");
+        System.out.println("    *   ");
+        System.out.println("  *     ");
+        System.out.println("* * * * *");
+
+    }
+}
